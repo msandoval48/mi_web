f48bb0663e2fa5d33d16bcf84b96286393baf345
Cristhian <Cristhian@CRIS.home>
2018-05-09 21:07:38 -0500

    Merge origin/master
    Conflicts:
    	web/WEB-INF/web.xml

--- web_ventas/index.php
+++ web_ventas/index.php
@@ -11,7 +11,9 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-                echo "Hola como estas"
+                echo "Hola como estas";
+            echo "<br>";
+                echo "CAmbio echo desde otra pc";
         ?>
     </body>
 </html>
